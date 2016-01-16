f0a1f4aa6a580d8f86b036f81b50cdbac41da330
James Boehmer <james.boehmer@mlb.com>
2016-01-15 19:27:52 -0500

    Added local dev instructions to readme

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Electronic Application Transformation - a model electronic application for the N
 # Code of Conduct
 This project adheres to the [Open Code of Conduct][code-of-conduct]. By participating, you are expected to honor this code.
 [code-of-conduct]: code-of-conduct.md
+
+# Prerequisites
+1. Python 2.7
+2. Mongodb
+
+# Running the app locally
+1. create and activate your virtualenv
+2. pip install -r requirements
+3. cd src && python manage.py runserver
---
+++ src/eat/models/application.py
@@ -0,0 +1,59 @@
+import datetime
+
+from argon2 import PasswordHasher
+
+from . import db
+
+
+class User(db.Document):
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow(), required=True)
+    email = db.EmailField(unique=True, required=True)
+    email_verified_at = db.DateTimeField(default=None, required=False)
+    password_hash = db.StringField(max_length=255, required=True)
+
+    first_name = db.StringField(max_length=255, required=False)
+    middle_initial = db.StringField(max_length=255, required=False)
+    last_name = db.StringField(max_length=255, required=False)
+
+    @property
+    def password(self):
+        return self.password_hash
+
+    @password.setter
+    def password(self, password):
+        hasher = PasswordHasher()
+        ph = hasher.hash(password)
+        self.password_hash = ph
+
+    def verify_password(self, password):
+        hasher = PasswordHasher()
+        try:
+            hasher.verify(self.password_hash, password)
+            return True
+        except Exception:
+            return False
+
+    def __unicode__(self):
+        return self.email
+
+    meta = {
+        'allow_inheritance': True,
+        'indexes': ['-created_at', 'email'],
+        'ordering': ['-created_at']
+    }
