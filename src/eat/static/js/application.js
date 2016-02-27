6a6b42497a8ff42a45e670df8519d870a1197408
James Boehmer <james.boehmer@mlb.com>
2016-02-26 20:10:06 -0500

    Fixed extra spaces in input attributes

--- src/eat/static/js/application.js
+++ src/eat/static/js/application.js
@@ -177,12 +177,12 @@ function children_form_handler(e) {
                                     )
                                     .append($('<form>').attr('child_id', child._id).attr('action', action="/svc/eat/v1/application/children/" + child._id).addClass('children_form')
                                         .append($('<div>').html('<label for="first_name">' + labelLookup('first_name') + '</label>: <input id="first_name" name="first_name" type="text" value="' + child.first_name+ '">'))
-                                        .append($('<div>').html('<label for="middle_initial">' + labelLookup('middle_initial') + '</label>: <input id="middle_initial " name="middle_initial " type="text" value="' + (child.middle_initial || '') + '">'))
-                                        .append($('<div>').html('<label for="last_name">' + labelLookup('last_name') + '</label>: <input id="last_name " name="last_name " type="text" value="' + child.last_name + '">'))
-                                        .append($('<div>').html('<label for="school_city">' + labelLookup('school_city') + '</label>: <input id="school_city " name="school_city " type="text" value="' + child.school_city + '">'))
-                                        .append($('<div>').html('<label for="school_state">' + labelLookup('school_state') + '</label>: <input id="school_state " name="school_state " type="text" value="' + child.school_state + '">'))
-                                        .append($('<div>').html('<label for="school_postal">' + labelLookup('school_postal') + '</label>: <input id="school_postal " name="school_postal " type="text" value="' + child.school_postal + '">'))
-                                        .append($('<div>').html('<label for="school_name">' + labelLookup('school_name') + '</label>: <input id="school_name " name="school_name " type="text" value="' + child.school_name + '">'))
+                                        .append($('<div>').html('<label for="middle_initial">' + labelLookup('middle_initial') + '</label>: <input id="middle_initial" name="middle_initial" type="text" value="' + (child.middle_initial || '') + '">'))
+                                        .append($('<div>').html('<label for="last_name">' + labelLookup('last_name') + '</label>: <input id="last_name" name="last_name" type="text" value="' + child.last_name + '">'))
+                                        .append($('<div>').html('<label for="school_city">' + labelLookup('school_city') + '</label>: <input id="school_city" name="school_city" type="text" value="' + child.school_city + '">'))
+                                        .append($('<div>').html('<label for="school_state">' + labelLookup('school_state') + '</label>: <input id="school_state" name="school_state" type="text" value="' + child.school_state + '">'))
+                                        .append($('<div>').html('<label for="school_postal">' + labelLookup('school_postal') + '</label>: <input id="school_postal" name="school_postal" type="text" value="' + child.school_postal + '">'))
+                                        .append($('<div>').html('<label for="school_name">' + labelLookup('school_name') + '</label>: <input id="school_name" name="school_name" type="text" value="' + child.school_name + '">'))
                                         .append($('<button>').attr('type', 'submit').text('Update'))
                                     );
 
