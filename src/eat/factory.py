9a89b297e6f52516258c8d57541ca646d4e9b52e
James Boehmer <james.boehmer@mlb.com>
2016-02-28 20:31:14 -0500

    Load settings silently

--- src/eat/factory.py
+++ src/eat/factory.py
@@ -11,7 +11,7 @@ def create_app(config=None, environment=None):
     app.config[
         "SECRET_KEY"] = "\x1a\xb1\x9d\x1d\xf2\x01\xa1X\xb8g\xed\x1c\xb3\x0f+s\xbce\xf6\x92\x83'\xf2\xbc\x96\xc6\x18\x03`\xc0\x0c("
     app.config["IV"] = '\xe7\x9d\xc7\xbd\x12l\x88\xc7\xe9D\x93!\xa2B\xed\x91'
-    app.config.from_pyfile('settings.cfg')
+    app.config.from_pyfile('settings.cfg', silent=True)
     app.session_interface = MongoSessionInterface(**(app.config["MONGODB_SETTINGS"]))
 
     with app.app_context():
